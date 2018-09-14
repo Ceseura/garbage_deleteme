7dd27747a331fe8ab2062e88bca01c76cbf74698
Alex Liang <unpiraticalturtle@gmail.com>
2018-09-14 14:59:15 -0400

    update functionality

--- blog/urls.py
+++ blog/urls.py
@@ -3,9 +3,11 @@ from django.conf.urls import url
 from .views import post_model_list_view
 from .views import post_model_create_view
 from .views import post_model_detail_view
+from .views import post_model_update_view
 
 urlpatterns = [
 	url(r'^$', post_model_list_view, name='list'),
 	url(r'^create/$', post_model_create_view, name='create'),
-	url(r'^(?P<id>\d+)/$', post_model_detail_view, name='detail')
+	url(r'^(?P<id>\d+)/$', post_model_detail_view, name='detail'),
+	url(r'^(?P<id>\d+)/update/$', post_model_update_view, name='update'),
 ]
